51bc58537f13b2272fe2fd37e9704fa76b1d59d9
Jeong-jin-Han <goldmonkey33@naver.com>
2025-02-17 17:50:52 +0900

    add

--- myapp/svelte.config.js
+++ myapp/svelte.config.js
@@ -1,3 +1,15 @@
+import adapter from '@sveltejs/adapter-vercel';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter()
+	}
+};
+
+export default config;
+
+
 // import adapter from '@sveltejs/adapter-auto';
 
 // /** @type {import('@sveltejs/kit').Config} */
@@ -11,14 +23,3 @@
 // };
 
 // export default config;
-
-import adapter from '@sveltejs/adapter-vercel';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter()
-	}
-};
-
-export default config;
